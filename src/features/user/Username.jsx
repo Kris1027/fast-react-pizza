4c867645bd212b814f4e59d6c24b37498f6c117a
Kris1027 <obarzanek.work@gmail.com>
2023-09-04 11:49:21 +0200

    Using flexbox

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Krzysztof</div>;
+  return <div className="hidden text-sm font-semibold md:block">Krzysztof</div>;
 }
 
 export default Username;
