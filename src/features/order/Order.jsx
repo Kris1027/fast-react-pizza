4ae76f2b8ed9a4fa8ace6ba05de5e2147146941d
Kris1027 <obarzanek.work@gmail.com>
2023-09-08 10:46:18 +0200

    Fetching Data without navigation useFetcher

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,7 +1,7 @@
-// Test ID: IIDSAT
+import { useFetcher, useLoaderData } from 'react-router-dom';
+import { useEffect } from 'react';
 
 import OrderItem from './OrderItem';
-import { useLoaderData } from 'react-router-dom';
 import { getOrder } from '../../services/apiRestaurant';
 import {
   calcMinutesLeft,
@@ -11,6 +11,16 @@ import {
 
 function Order() {
   const order = useLoaderData();
+
+  const fetcher = useFetcher();
+
+  useEffect(
+    function () {
+      if (!fetcher.data && fetcher.state === 'idle') fetcher.load('/menu');
+    },
+    [fetcher],
+  );
+
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
     id,
@@ -53,7 +63,15 @@ function Order() {
 
       <ul className="dive-stone-200 divide-y border-b border-t">
         {cart.map((item) => (
-          <OrderItem item={item} key={item.pizzaId} />
+          <OrderItem
+            item={item}
+            key={item.pizzaId}
+            isLoadingIngredients={fetcher.state === 'loading'}
+            ingredients={
+              fetcher?.data?.find((el) => el.id === item.pizzaId)
+                ?.ingredients ?? []
+            }
+          />
         ))}
       </ul>
 
